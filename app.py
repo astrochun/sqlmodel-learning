53f07f8e464fc70c7846b92f53261f97c77e384b
Chun Ly <astro.chun@gmail.com>
2022-05-25 16:16:48 -0700

    Add records with create_heroes(), refactor into main()

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python
 
+from sqlmodel import Session
 from sqlmodel_learning.db import SQLModel, engine
+from sqlmodel_learning.models.hero import Hero
 
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-if __name__ == "__main__":
+def create_heroes():
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+    session = Session(engine)
+
+    session.add(hero_1)
+    session.add(hero_2)
+    session.add(hero_3)
+
+    session.commit()
+
+    session.close()
+
+
+def main():
     create_db_and_tables()
+    create_heroes()
+
+
+if __name__ == "__main__":
+    main()
